5521d44e569bba1a0c982b3cdd2820bab95c01be
brysonwaisi <brysonnyamwange@gmail.com>
2022-03-10 12:09:20 -0800

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+
 /**
- * main- the main block
  *
- * return: 0
+ *  * main -the main block
  *
- */
+ *   * Return: 0
+ *
+ *    */
 
 int main(void)
 
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return(0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
