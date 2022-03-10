dcaa25e3bb1e20000ade2d9c1dc99804ef78f9f2
brysonwaisi <brysonnyamwange@gmail.com>
2022-03-10 13:51:45 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
 #include <unistd.h>
-
 /**
  * main - Entry point
  * Return:1
@@ -10,10 +8,8 @@
 int main(void)
 {
 
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	write(2, ch, sizeof(ch));
-
-	return (1);
+	fprintf(stderr,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+			return (1);
 
 }
