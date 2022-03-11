251bd0618a103f9ddc8b48ab52eb08c28ceef5c3
brysonwaisi <brysonnyamwange@gmail.com>
2022-03-11 02:32:42 -0800

    dont use char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int t;
+
+	for (t = '0'; t <= '9'; t++)
+		putchar(t);
+		putchar('\n');
+
+		return (0);
+}
+
