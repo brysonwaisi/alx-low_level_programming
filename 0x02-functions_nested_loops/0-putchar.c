cb398dba8cc4498e4523262ea5475ba093fb6446
brysonwaisi <brysonnyamwange@gmail.com>
2022-03-15 07:41:07 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -13,7 +11,15 @@
 int main(void)
 
 {
-	_putchar("_putchar\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 
 	return (0);
 }
