250e2073cf5b4d4d559561cfa9573f6e537895d0
brysonwaisi <brysonnyamwange@gmail.com>
2022-03-18 03:36:54 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - print numbers 0 to 9
- * Return: 0
+ * Return: 0 
  */
 
 void print_numbers(void)
@@ -10,9 +10,7 @@ void print_numbers(void)
 	int num;
 
 	for (num = 0; num <= 9; num++)
-	{
 		_putchar((num % 10) + '0');
-	}
-	_putchar('\n');
 
+	_putchar('\n');
 }
