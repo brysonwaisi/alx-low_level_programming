18f2d375426abe84fdacad6628808b900bd45816
brysonwaisi <brysonnyamwange@gmail.com>
2022-03-24 13:37:13 -0700

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,10 +2,10 @@
 
 /**
  * strcat - Concatenates the string pointed to by @src
- * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
+ * @dest: A pointer to the string to be concatenated
+ * @src: The source string to be appended to @dest
  *
- * Return: A pointer to the destination string @dest.
+ * Return: pointer to @dest
  */
 
 char *_strcat(char *dest, char *src)
