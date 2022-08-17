f79c1d7e29a10216e50e0bb1cefc3d45c20f1b6e
brysonwaisi <brysonnyamwange@gmail.com>
2022-08-17 08:12:08 -0700

    Technical Interview Prep

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines an island perimeter measuring function."""
 
+
 def island_perimeter(grid):
     """Return the perimiter of an island.
     The grid represents water by 0 and land by 1.
@@ -14,11 +15,11 @@ def island_perimeter(grid):
     edges = 0
     size = 0
 
-    for in in range(height):
+    for i in range(height):
         for j in range(width):
             if grid[i][j] == 1:
                 size += 1
-                if (j > 0 and grid[i][j-1] ==1):
+                if (j > 0 and grid[i][j - 1] == 1):
                     edges += 1
                 if (i > 0 and grid[i - 1][j] == 1):
                     edges += 1
